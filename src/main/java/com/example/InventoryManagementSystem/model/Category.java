8a761199b3406558f961432b8591e96f43ea444e
Pandas-12 <thantsithu.naing@icloud.com>
2025-02-02 21:58:36 +0700

    model and error

--- src/main/java/com/example/InventoryManagementSystem/model/Category.java
+++ src/main/java/com/example/InventoryManagementSystem/model/Category.java
@@ -1,2 +1,15 @@
-package com.example.InventoryManagementSystem.model;public class Category {
+package com.example.InventoryManagementSystem.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CategoryName name;
 }
--- src/main/java/com/example/InventoryManagementSystem/model/Lot.java
+++ src/main/java/com/example/InventoryManagementSystem/model/Lot.java
@@ -1,2 +1,26 @@
-package com.example.InventoryManagementSystem.model;public class Lot {
+package com.example.InventoryManagementSystem.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "lot")
+public class Lot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LotStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "warehouse_id", nullable = false)
+    private Warehouse warehouse;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
 }
